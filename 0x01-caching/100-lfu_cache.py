2269a9eee3ba0128de35718670a251deac859e75
nobleenia <neluwah@gmail.com>
2024-05-08 15:34:18 +0000

    Corrections

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,59 +1,68 @@
 #!/usr/bin/env python3
 """100-lfu_cache module"""
 from base_caching import BaseCaching
+from collections import defaultdict, OrderedDict
 
 
 class LFUCache(BaseCaching):
     """
-    LFUCache class that inherits from BaseCaching and is a caching system
-    that discards the least frequently used items.
+    LFUCache class that inherits from BaseCaching, is a caching
+    system that discards the least frequently used items.
     """
 
     def __init__(self):
         """Initialize"""
         super().__init__()
         self.usage_frequency = {}
-        self.time_stamp = {}
+        self.items = OrderedDict()
 
     def put(self, key, item):
         """
         Assign the item value for the key in self.cache_data.
-        Discard the least frequency used item or the LRU if there is a tie.
+        Discard least frequent item or the LRU if there is a tie.
         """
         if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS and \
+               key not in self.cache_data:
+                self.evict()
             self.cache_data[key] = item
-            if key in self.usage_frequency:
-                self.usage_frequency[key] += 1
+            self.usage_frequency[key] += 1
+            # Move the key to the end to maintain LRU order
+            if key in self.items:
+                self.items.move_to_end(key)
             else:
-                self.usage_frequency[key] = 1
-            self.time_stamp[key] = self.current_time_stamp()
-
-            if len(self.cache_data) > self.MAX_ITEMS:
-                least_freq = min(self.usage_frequency.values())
-                least_freq_keys = [k for k, v in self.usage_frequency.items() if v == least_freq]
-                if len(least_freq_keys) == 1:
-                    lfu_key = least_freq_keys[0]
-                else:
-                    # Tie situation: use LRU
-                    lfu_key = sorted(least_freq_keys, key=lambda k: self.time_stamp[k])[0]
-
-                self.cache_data.pop(lfu_key)
-                self.usage_frequency.pop(lfu_key)
-                self.time_stamp.pop(lfu_key)
-                print(f"DISCARD: {lfu_key}")
+                self.items[key] = None
 
     def get(self, key):
         """
         Return the value linked to key in self.cache_data.
-        Increase frequency, update the timestamp, and return the value.
+        Increase frequency, update the timestamp,
+        and return the value.
         """
         if key in self.cache_data:
             self.usage_frequency[key] += 1
-            self.time_stamp[key] = self.current_time_stamp()
+            self.items.move_to_end(key)
             return self.cache_data[key]
         return None
 
-    def current_time_stamp(self):
-        """Return a monotonically increasing time stamp."""
-        from time import monotonic
-        return monotonic()
+    def evict(self):
+        """
+        Evict items based on LFU policy, and in case of a tie,
+        based on LRU policy.
+        """
+        if not self.items:
+            return
+        # Find the least frequently used item
+        least_freq = min(self.usage_frequency.values())
+        # Collect all candidates with the same least frequency
+        least_freq_keys = [
+            k for k, v in self.usage_frequency.items() if v == least_freq
+        ]
+        # Use the first inserted item for tie-breaking
+        for key in self.items:
+            if key in least_freq_keys:
+                self.cache_data.pop(key, None)
+                self.usage_frequency.pop(key, None)
+                self.items.pop(key, None)
+                print(f"DISCARD: {key}")
+                break
